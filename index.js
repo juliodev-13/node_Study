c6db2e98d0107f0681bab44a44991ed946f92ea4
Júlio C. Sabiá <79371902+juliodev-13@users.noreply.github.com>
2022-07-05 22:50:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// const { Person } = require("../person");
+const dotenv = require("dotenv");
+
+const connectToDataBase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDataBase();
+
+require("./modules/express");
+// const person = new Person("Julio");
